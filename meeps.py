e6a725a31e8d53ba3c43efdaec1794e8733babfa
UncleSocks <ilisan.tyrone@gmail.com>
2025-07-05 11:32:41 +0800

    Update: Refactored the "meeps.py" file.

--- meeps.py
+++ meeps.py
@@ -1,98 +1,91 @@
 import pygame
 import pygame_gui
+
 import init
 import elements.main_menu as main_menu_element
 from game_loops.shift import shift_introduction
-from game_loops.tickets import ticket_management   
+from game_loops.tickets import ticket_management
 from game_loops.accounts import accounts_management
 from game_loops.threats import threat_database_management
 
 
-def main_menu():
 
-    
-    def music_init():
+class MainMenu:
 
-        menu_button_music_path = "assets/sounds/menu_button.mp3"
-        pygame.mixer.music.load(menu_button_music_path)
-        menu_button_music_channel = pygame.mixer.Channel(0)
+    def __init__ (self):
 
-        return menu_button_music_path, menu_button_music_channel
-    
-    
-    def main_menu_init():
+        #Initializes SQLite database connection and cursor.
+        self.database = 'data.db'
+        self.connect, self.cursor = init.database_init(self.database)
 
-        database = 'data.db'
-        connect, cursor = init.database_init(database)
+        #Initializes PyGame and window(s). 
+        self.window_surface, self.clock, self.background = init.pygame_init()
+        self.manager = init.pygame_gui_init()
 
-        window_surface, clock, background = init.pygame_init()
-        manager = init.pygame_gui_init()
+        #Displays main menu buttons.
+        self.start_button = main_menu_element.start_button_func(self.manager)
+        self.ticket_management_button = main_menu_element.ticket_management_button_func(self.manager)
+        self.account_management_button = main_menu_element.accounts_management_button_func(self.manager)
+        self.threat_entries_button = main_menu_element.threat_entries_button_func(self.manager)
+        self.quit_button = main_menu_element.quit_button_func(self.manager)
 
-        start_button = main_menu_element.start_button_func(manager)
-        ticket_management_button = main_menu_element.ticket_management_button_func(manager)
-        account_management_button = main_menu_element.accounts_management_button_func(manager)
-        threat_entries_button = main_menu_element.threat_entries_button_func(manager)
-        quit_button = main_menu_element.quit_button_func(manager)
+        #Initializes and displays miscellaneous assets.
+        self.title_image_path = 'assets/images/general/title.png'
+        main_menu_element.main_title_image_func(self.manager, self.title_image_path)
+        main_menu_element.main_title_slogan_label_func(self.manager)
 
-        title_image_path = "assets/images/general/title.png"
-        main_title_image = main_menu_element.main_title_image_func(manager, title_image_path)
-        main_title_slogan = main_menu_element.main_title_slogan_label_func(manager)
+        self.current_version = "v2025.0.1 BETA"
+        main_menu_element.version_label_func(self.manager, self.current_version)
+        main_menu_element.github_label_func(self.manager)
 
-        current_version = "v2024.0.1 BETA"
-        version_label = main_menu_element.version_label_func(manager, current_version)
-        github_label = main_menu_element.github_label_func(manager)
-
-        return main_menu_loop(connect, cursor, window_surface, clock, background, manager, start_button, 
-                              ticket_management_button, account_management_button, 
-                              threat_entries_button, quit_button, main_title_image, 
-                              main_title_slogan, version_label, github_label)
-
-
-    def main_menu_loop(connect, cursor, window_surface, clock, background, manager, 
-                       start_button, ticket_management_button, account_management_button, 
-                       threat_entries_button, quit_button, main_title_image, 
-                       main_title_slogan, version_label, github_label):
         
-        menu_button_music_path, menu_button_music_channel = music_init()
+        #Initializes and loads main menu music.
+        self.menu_button_music_path = 'assets/sounds/menu_button.mp3'
+        pygame.mixer.music.load(self.menu_button_music_path)
+        self.menu_button_music_channel = pygame.mixer.Channel(0)
+
+
+    def main_menu_loop (self):
 
         running = True
         while running:
-            time_delta = clock.tick(60) / 1000.0
+
+            time_delta = self.clock.tick(60) / 1000.0
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
                 if event.type == pygame_gui.UI_BUTTON_PRESSED:
-                    menu_button_music_channel.play(pygame.mixer.Sound(menu_button_music_path))
-                    if event.ui_element == start_button:
-                        shift_introduction(connect, cursor)
+                    self.menu_button_music_channel.play(pygame.mixer.Sound(self.menu_button_music_path))
 
-                    elif event.ui_element == ticket_management_button:
-                        ticket_management(connect, cursor)
+                    if event.ui_element == self.start_button:
+                        shift_introduction(self.connect, self.cursor)
 
-                    elif event.ui_element == account_management_button:
-                        accounts_management(connect, cursor)
-                    
-                    elif event.ui_element == threat_entries_button:
-                        threat_database_management(connect, cursor)
+                    elif event.ui_element == self.ticket_management_button:
+                        ticket_management(self.connect, self.cursor)
 
-                    elif event.ui_element == quit_button:
+                    elif event.ui_element == self.account_management_button:
+                        accounts_management(self.connect, self.cursor)
+
+                    elif event.ui_element == self.threat_entries_button:
+                        threat_database_management(self.connect, self.cursor)
+
+                    elif event.ui_element == self.quit_button:
                         running = False
 
-                manager.process_events(event)
+                self.manager.process_events(event)
 
-            manager.update(time_delta)
+            self.manager.update(time_delta)
 
-            window_surface.blit(background, (0, 0))
-            manager.draw_ui(window_surface)
+            self.window_surface.blit(self.background, (0,0))
+            self.manager.draw_ui(self.window_surface)
             pygame.display.update()
-        
-        connect.close()
-        pygame.quit()
 
-    main_menu_init()
+        self.connect.close()
+        pygame.quit()
 
 
 if __name__ == "__main__":
-
-    main_menu()
+    main_menu = MainMenu()
+    main_menu.main_menu_loop()
